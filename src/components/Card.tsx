02b3002ffd848890d46069e5b02778879fdcd28e
Sanne Lindholm <sanne@meliohealth.com>
2022-06-27 13:52:50 +0200

    Refactoring style options for card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -22,36 +22,34 @@ export interface CardProps extends React.HTMLAttributes<HTMLElement> {
 const Card = ({contentInfo, className, cardSizeLarge}: CardProps) => {
   const {name, description, sponsored, url, image} = contentInfo;
 
+  const styleOption = cardSizeLarge
+    ? {
+        wrapper: styles.bigImageWrapper,
+        gradient: styles.whiteGradient,
+        textWrapper: styles.whiteTextWrapper,
+        infoWrapper: styles.whiteInfoWrapper,
+        title: styles.blackTitle,
+      }
+    : {
+        wrapper: styles.smallImageWrapper,
+        gradient: styles.blackGradient,
+        textWrapper: styles.blackTextWrapper,
+        infoWrapper: styles.blackInfoWrapper,
+        title: styles.whiteTitle,
+      };
+
   return (
     <div className={`${styles.wrapper} ${className}`}>
       <a className={styles.link} href={`http://www.köket.se${url}`}>
         Läs mer
       </a>
-      <div
-        className={
-          cardSizeLarge ? styles.bigImageWrapper : styles.smallImageWrapper
-        }
-      >
+      <div className={styleOption.wrapper}>
         <img src={image.url} className={styles.image} alt={image.name} />
-        <div
-          className={
-            cardSizeLarge ? styles.whiteGradient : styles.blackGradient
-          }
-        />
+        <div className={styleOption.gradient} />
       </div>
-      <div
-        className={
-          cardSizeLarge ? styles.whiteTextWrapper : styles.blackTextWrapper
-        }
-      >
-        <div
-          className={
-            cardSizeLarge ? styles.whiteInfoWrapper : styles.blackInfoWrapper
-          }
-        >
-          <h1 className={cardSizeLarge ? styles.blackTitle : styles.whiteTitle}>
-            {name}
-          </h1>
+      <div className={styleOption.textWrapper}>
+        <div className={styleOption.infoWrapper}>
+          <h1 className={styleOption.title}>{name}</h1>
           {cardSizeLarge && <p className={styles.description}>{description}</p>}
         </div>
       </div>
